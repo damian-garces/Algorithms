2b66ea9487edb39b4381026f180b371d03822284
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-02-17 21:09:27 +0400

    Modify .gitignore

--- CSharp/Algorithms/obj/Debug/net9.0/Algorithms.AssemblyInfo.cs
+++ CSharp/Algorithms/obj/Debug/net9.0/Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2270aa27a0c187d0bea0fef7f32b0b3f4001170a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f444892d67138ee3a3e3310f567ba436613f495")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
