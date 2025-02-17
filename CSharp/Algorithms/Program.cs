3b018990a24350d7ef8c87b0afc24cfc703d2ff4
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-02-17 21:20:52 +0400

    Modify messages in console write line

--- CSharp/Algorithms/Program.cs
+++ CSharp/Algorithms/Program.cs
@@ -3,16 +3,16 @@ List<string> words1 = ["habito", "hacer", "lectura", "sonreir"];
 string order1 = "hlabcdfgijkmnopqrstuvwxyz";
 
 var isAlienSorted =  new AlienDictionary().IsAlienSorted(words1, order1);
-Console.WriteLine($"The words is sorted: {isAlienSorted}");
+Console.WriteLine($"The words 1 is sorted: {isAlienSorted}");
 
 List<string> words2 = ["habito", "hacer", "sonreir", "lectura"];
 string order2 = "hlabcdfgijkmnopqrstuvwxyz";
 
 isAlienSorted =  new AlienDictionary().IsAlienSorted(words2, order2);
-Console.WriteLine($"The words is sorted: {isAlienSorted}");
+Console.WriteLine($"The words 2 is sorted: {isAlienSorted}");
 
 List<string> words3 = ["conocer", "cono"];
 string order3 = "abcdefghijkmnopqrstuvwxyz";
 
 isAlienSorted =  new AlienDictionary().IsAlienSorted(words3, order3);
-Console.WriteLine($"The words is sorted: {isAlienSorted}");
+Console.WriteLine($"The words 3 is sorted: {isAlienSorted}");
